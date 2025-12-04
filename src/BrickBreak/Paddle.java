f4059e3d08cf10df8c9931fc50c99f203b2d9d0d
Fedor Menchukov <77518360+menchukf@users.noreply.github.com>
2025-12-04 14:47:16 -0500

    Add Paddle class skeleton
    Created Paddle class with placeholders for code.

---
+++ src/BrickBreak/Paddle.java
@@ -0,0 +1,19 @@
+import java.awt.Graphics;
+import java.awt.*;
+
+public class Paddle {
+
+
+	//your code here!
+
+
+	
+	//don't forget you need instance variables:
+	
+	
+	//constructor(s):
+	
+	//methods:
+	
+	
+}
